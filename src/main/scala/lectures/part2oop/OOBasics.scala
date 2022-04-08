b81485c5bb01559191061fd208fb03f83a8cc147
Cristian David Chavez Quintero <cristian.chavez@jikkosoft.com>
2022-04-07 21:00:36 -0500

    end of the video

--- src/main/scala/lectures/part2oop/OOBasics.scala
+++ src/main/scala/lectures/part2oop/OOBasics.scala
@@ -2,9 +2,31 @@ package lectures.part2oop
 
 object OOBasics extends App 
 {
-    val person = new Person
-    println(person)
+    val person = new Person("Cristian",26)
+    println(person.age)
+    println(person.x)
+    println(person.greet("Daniel"))
+    println(person.greet())
+}
+
+// constructor 
+class Person(name:String, val age:Int) 
+{
+    // body 
+    // 
+    val x = 2 
+
+    println(1 + 3)
+
+    def greet(name:String):Unit =  println(s"${this.name} says: Hi, $name")
+
+
+    // overloading
+    def greet():Unit = println(s"Hi, I am $name")
+
+
+    def this(name:String) = this(name,0)
 }
 
 
-class Person
+// class parameters are NOT FIELDS 
