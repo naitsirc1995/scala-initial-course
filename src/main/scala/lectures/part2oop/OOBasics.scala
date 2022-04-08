4b664a6caddbb8f07ac4cbe76d3fb38ae0e942ad
Cristian David Chavez Quintero <cristian.chavez@jikkosoft.com>
2022-04-07 21:11:52 -0500

    first branch commit

--- src/main/scala/lectures/part2oop/OOBasics.scala
+++ src/main/scala/lectures/part2oop/OOBasics.scala
@@ -7,6 +7,10 @@ object OOBasics extends App
     println(person.x)
     println(person.greet("Daniel"))
     println(person.greet())
+
+    val someWriter = new Writer("Paul","Dickinson",1982)
+
+    println(someWriter.fullName)
 }
 
 // constructor 
@@ -28,5 +32,42 @@ class Person(name:String, val age:Int)
     def this(name:String) = this(name,0)
 }
 
+/*
+
+Novel and a Writer class 
+
+Writer:first name, surname, year 
+ - method fullname 
+
+
+Novel : name, year of release, author 
+
+  - authorAge 
+  - isWrittenBy(author)
+  - copy  (new uear of release) = new instance of Novel
+
+
+
+Counter class 
+ - recieves an int value 
+ - method current count 
+ - method to increment/decrement => new counter 
+ - overload inc/dec to receive an amount 
+
+*/
+
+
 
 // class parameters are NOT FIELDS 
+
+
+class Writer(firstName:String, surname:String,year:Int)
+{
+    def fullName = firstName + " " + surname
+}
+
+
+class Novel(authorAge:Int,isWrittenBy:String)
+{
+    
+}
