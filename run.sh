58e50675a9379ab4403babe3123c42870dee9ffb
Cristian David Chavez Quintero <cristian.chavez@jikkosoft.com>
2022-04-10 21:41:06 -0500

    First branch commit

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-mvn scala:run -DmainClass=lectures.part3fp.WahtsAFunction -q
+mvn scala:run -DmainClass=lectures.part3fp.AnonymousFunctions -q
---
+++ target/classes/lectures/part3fp/AnonymousFunctions.scala
@@ -0,0 +1,6 @@
+package lectures.part3fp
+
+object AnonymousFunctions extends App 
+{
+    println("This is a great begining for something that you already knew")
+}
